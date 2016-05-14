25acbdb1008f047290c700db5e4d30cbf111a512
artemsky <mr.artemsky@gmail.com>
2016-05-14 13:55:48 +0300

    add custom pure scrollbar

--- dist/js/app.min.js
+++ dist/js/app.min.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 
-    $('input[type="range"]').rangeslider({
+    $('.evart-volume input[type="range"]').rangeslider({
 
         // Feature detection the default is `true`.
         // Set this to `false` if you want to use
@@ -10,12 +10,12 @@ $(document).ready(function () {
         polyfill: false,
 
         // Default CSS classes
-        rangeClass: 'rangeslider',
-        disabledClass: 'rangeslider--disabled',
-        horizontalClass: 'rangeslider--horizontal',
-        verticalClass: 'rangeslider--vertical',
-        fillClass: 'rangeslider__fill',
-        handleClass: 'rangeslider__handle__evart',
+        rangeClass: 'sound-rangeslider',
+        disabledClass: 'sound-rangeslider--disabled',
+        horizontalClass: 'sound-rangeslider--horizontal',
+        verticalClass: 'sound-rangeslider--vertical',
+        fillClass: 'sound-rangeslider__fill',
+        handleClass: 'sound-rangeslider__handle',
 
         // Callback function
         onInit: function() {
@@ -25,11 +25,11 @@ $(document).ready(function () {
         // Callback function
         onSlide: function(position, value) {
             var bg = "url('../img/volume-on.png')";
-            $(".evart-radio .evart-value").text(value + "%");
+            $(".evart-volume .evart-value").text(value + "%");
             if(value < 1)
                 bg = "url('../img/volume-off.png')";
 
-            $(".evart-ico").css({
+            $(".evart-volume .evart-ico").css({
                 backgroundImage: bg
             });
         },
@@ -39,5 +39,47 @@ $(document).ready(function () {
 
         }
     });
+
+
+    $('.evart-playlist input[type="range"]').rangeslider({
+
+        // Feature detection the default is `true`.
+        // Set this to `false` if you want to use
+        // the polyfill also in Browsers which support
+        // the native <input type="range"> element.
+        polyfill: false,
+
+        // Default CSS classes
+        rangeClass: 'playlist-rangeslider',
+        disabledClass: 'playlist-rangeslider--disabled',
+        horizontalClass: 'playlist-rangeslider--horizontal',
+        verticalClass: 'playlist-rangeslider--vertical',
+        fillClass: 'playlist-rangeslider__fill',
+        handleClass: 'playlist-rangeslider__handle',
+
+        // Callback function
+        onInit: function() {
+            //$(".evart-radio .evart-value").text($(this)[0].value + "%");
+        },
+
+        // Callback function
+        onSlide: function(position, value) {
+            // var bg = "url('../img/volume-on.png')";
+            // $(".evart-radio .evart-value").text(value + "%");
+            // if(value < 1)
+            //     bg = "url('../img/volume-off.png')";
+            //
+            // $(".evart-ico").css({
+            //     backgroundImage: bg
+            // });
+        },
+
+        // Callback function
+        onSlideEnd: function(position, value) {
+
+        }
+    });
+
+    // $('.evart-playlist-scroll').prop('scrollHeight');
 
 });
